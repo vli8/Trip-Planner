094368e9b9ac5332d3bf7854a894db4cdcfb5ef2
Victor Li <vli8@fordham.edu>
2018-06-19 17:30:18 -0400

    bug fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const mapboxgl = require('mapbox-gl');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmlsYWRlbGZvIiwiYSI6ImNqaW01aGk3cjAwa3Ezd28yeG5rdDA4ZG4ifQ.9cpbfyj_cB5Ikc2tMIBBdw';
 const map = new mapboxgl.Map({
-    container: "map",
+    container: "map",//map is the id = "#map" of the html file
     center: [-74.009, 40.705],//location of Full Stack
     zoom: 12, //Starting zoom
     style: "mapbox://styles/mapbox/streets-v10" //beginning styling template
