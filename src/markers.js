5a6c4cb5b492abc930277ff7ef412de62cb5b865
Filadelfo Braz <filadelfo.braz@gmail.com>
2018-06-19 17:29:22 -0400

    working on function to display icons

--- src/markers.js
+++ src/markers.js
@@ -7,6 +7,22 @@ const iconURLs = {
 };
 
 const buildMarker = (type, coords) => {
+  const marker = document.createElement("div");
+  marker.style.width = "32px";
+  marker.style.height = "39px";
+  if(type === 'hotels'){
+    marker.style.backgroundImage = `url(${iconURLs.hotels})`
+  }
+  else if(type === 'restaurants'){
+    marker.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)"
+  }
+  else {
+    marker.style.backgroundImage = `url(${iconURLs.activities})`
+  }
+
+  return new mapboxgl.Marker(marker).setLngLat(coords)
 
 }
+
+module.exports = buildMarker
 
